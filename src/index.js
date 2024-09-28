94a17caae5b77fe0e1aa28710ced1ba226bb1268
Chathura <chathura@Chathuras-Laptop.local>
2024-09-28 22:36:24 +0530

    test5

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { Command } from 'commander';
 import fs from 'fs-extra';
 import path from 'path';
 import ora from 'ora';
-import { componentTemplate, customHookTemplate } from './templates/dashboardTemplate';
+import { componentTemplate, customHookTemplate } from './templates/dashboardTemplate.js';
 
 const program = new Command();
 
